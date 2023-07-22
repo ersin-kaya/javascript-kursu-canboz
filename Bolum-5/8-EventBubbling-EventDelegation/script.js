ee892149c53a472f03a312371c81efe18d0402f0
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-23 00:46:37 +0300

    EventBubbling-EventDelegation(delegation'ı bir önceki committe uygulamışım)

--- Bolum-5/8-EventBubbling-EventDelegation/script.js
+++ Bolum-5/8-EventBubbling-EventDelegation/script.js
@@ -4,22 +4,42 @@ const button = document.querySelector('button');
 //     console.log('Tıklandı');
 // });
 
+
+//anlattıı.... li için yazdığımız eventi açtık, bu durumda hem bu hem de onun üstünde olan ul için yazılan event çalışıyordu, bunun önüne (asıl element li olduğu için ona yazdık) e.stopPropagation() ekleyerek geçmiş olduk.
+//ancaaaak, şimdi burada anladığım kadarıyla, bu iç içe olanların kabarcıklanmasına(bubbling) eventbubbling deniyor
+
+//!!!!!!şu yazıya kesinlike bak. bu dersin konularını net bir şekilde anlatıyor. KISACA ANLADIĞIM KADARIYLA BEN ASLINDA EVENTDELEGATİON OLAYINI BİR ÖNCEKİ COMMİTTE UL ETİKETİNE .REMOVE() YAPTIRARAK YAZMIŞ OLDUM
+// https://sinemsungur.medium.com/nedir-bu-event-delegation-bb0ce58f61db
+
+
+
+//Bu, önce en içteki eleman tarafından yakalanan ve sonra dış elemanlara yayılan Event Bubbling’tir.
+
+//Kutu-3'e tıkladığımızda önce Kutu-1'i, ardından Kutu-2'yi ve son olarak tıkladığımız elemanı tetikler. Capturing dediğimiz şey, yukarıdan aşağıya (olayı tetiklediğimiz öğe) doğrudur.
+
+//Default olarak Javascript, event yayılımını Bubble olarak ayarlar. Capturing kullanmak istiyorsak, addEventListener methodundaki 3. argümanı true olarak ayarlamamız gerekir.
+
+//Ek bilgi: stopPropagation (), Bubbling ve Capturing aşamalarında geçerli eventin daha fazla yayılmasını önler. Bir elementteki event tetiklendiğinde onun üst elementlerinde de olayın tetiklenmesinin önüne geçmek için kullanabilirsiniz.
+
+// Event delegationın en büyük faydası, yönetmek için daha az fonksiyon kullanırken aynı zamanda hafızada daha az yer kaplamasıdır!(anladığım kadarıyla eventi her li için değil de ul altında yazmamız yani eventi parent'a eklememiz, yukarıdaki linkteki yazıyı incele anlarsın)
+
 //tıklanan elemanın listeden silinmesi
-// const liElemanlari = document.querySelectorAll('li');
-// liElemanlari.forEach(liElemani => {
-//     liElemani.addEventListener('click', e => {
-//         // console.log(`${liElemani.textContent} tiklandi`); // e yerine () yazınca böyle erişmiştik
-//         // console.log(e);
-//         // console.log(e.target);
-//         e.target.remove();
-//     });
-// });
+const liElemanlari = document.querySelectorAll('li');
+liElemanlari.forEach(liElemani => {
+    liElemani.addEventListener('click', e => {
+        // console.log(`${liElemani.textContent} tiklandi`); // e yerine () yazınca böyle erişmiştik
+        // console.log(e);
+        e.stopPropagation();
+        console.log(e.target);
+        e.target.remove();
+    });
+});
 
 const ul = document.querySelector('ul');
 // ul.remove();
 ul.addEventListener('click', e => {
     // console.log(e);
-    // console.log(e.target);
+    console.log(e.target);
     if (e.target.tagName === 'LI') {
         e.target.remove();
     }
