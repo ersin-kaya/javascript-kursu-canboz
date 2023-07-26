c1aa59e2c78fd44c5a3b7ca8cc5523c56ea0792d
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-27 01:52:41 +0300

    To Do App - liste elemanları içerisinda arama yapma

--- Bolum-10/1-ToDoApp/script.js
+++ Bolum-10/1-ToDoApp/script.js
@@ -1,5 +1,9 @@
 const addForm = document.querySelector('.add');
 const listUl = document.querySelector('.todos');
+const searchInput = document.querySelector('.search input'); //böyle veya..
+// const searchInput2 = document.querySelector('.search').search; //search form'unun altındaki search name'li input fakat bu çok okunaklı değil gibi şu an
+// console.log(searchInput.outerHTML);
+// console.log(searchInput2.outerHTML);
 
 const generateTemplate = toDo => {
     const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
@@ -25,4 +29,29 @@ listUl.addEventListener('click', event => {
     if (event.target.classList.contains('delete')) {
         event.target.parentElement.remove();
     }
+});
+
+const filterToDos = searchValue => {
+    // console.log(searchValue);
+    // console.log(listUl.children);
+    // console.log(Array.from(listUl.children));
+    const listElements = Array.from(listUl.children);
+    // console.log(listElements);
+
+    //içermeyenleri gizlemek için
+    // console.log(listElements.filter(toDo => !toDo.textContent.includes(searchValue))); //ile içermeyenleri alıyoruz
+    listElements.filter(toDo => !toDo.textContent.toLowerCase().includes(searchValue))
+        .forEach(toDo => toDo.classList.add('filtered')); //ardından foreach ile bunları dönerek filtered class'ını ekliyoruz //aşağıda not yazıyor, büyük-küçük harf kontrolünü atlatmak için toLowerCase
+
+
+    //içerenleri göstermek için (örneğin qwertyxyz girdin hepsi gizlendi ama silince gelmiyordu...)
+    listElements.filter(toDo => toDo.textContent.toLowerCase().includes(searchValue))
+        .forEach(toDo => toDo.classList.remove('filtered')); //aşağıda not yazıyor, büyük-küçük harf kontrolünü atlatmak için toLowerCase
+
+};
+
+searchInput.addEventListener('keyup', () => {
+    const searchValue = searchInput.value.trim().toLowerCase(); //büyük-küçük harf kontrolünün önüne geçmek için gönderirkende kontrol ederkende örneğin küçük harf olarak bakacağız o yüzden toLowerCase()
+    // console.log(searchValue);
+    filterToDos(searchValue);
 });
