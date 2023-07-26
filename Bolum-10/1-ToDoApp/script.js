f6ed3aec698eb34055cb05bbd9a4e28ce5935e77
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-27 01:01:27 +0300

    To Do App - ekleme işlemi tamamlandı

--- Bolum-10/1-ToDoApp/script.js
+++ Bolum-10/1-ToDoApp/script.js
@@ -1,9 +1,20 @@
 const addForm = document.querySelector('.add');
+const listUl = document.querySelector('.todos');
 
-
+const generateTemplate = toDo => {
+    const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${toDo}</span>
+    <i class="bi bi-trash3 delete"></i>
+</li>`;
+    return html;
+};
 
 addForm.addEventListener('submit', event => {
     event.preventDefault();
     const newToDo = addForm.add.value.trim(); //trimStart() + trimEnd() = trim() //baştaki ve sondaki boşlukları kırpar
-    console.log(newToDo);
+    // console.log(newToDo);
+    if (newToDo.length) { //length 0 değilse true döner, 0 ise false döner
+        listUl.innerHTML += generateTemplate(newToDo);
+        addForm.reset();
+    }
 });
