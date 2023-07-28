a7e9d65f6713ed0e34929576bf103be2dbde556c
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-28 23:35:52 +0300

    Callback Problem - ilk json dosyamıza istek attık ve datayı aldık

--- Bolum-11/6-CallbackProblem/script.js
+++ Bolum-11/6-CallbackProblem/script.js
@@ -1,4 +1,4 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
     request.addEventListener('readystatechange', () => {
         if (request.readyState === 4 && request.status === 200) {
@@ -9,14 +9,12 @@ const getTodos = (callback) => {
         }
     });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    request.open('GET', resource); //1. istek attığımız yer artık bu adres değil:'https://jsonplaceholder.typicode.com/todos'
     request.send();
 };
 
-getTodos((err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+getTodos('example/can.json', (err, data) => { //2. o halde buradaki callbackfunction yapısıda değişmiş olacak. İlk parametre bizim dosyayı açacağımız yer olacak.
+
+    console.log(data); //3.artık bir error'a gerek yok diyor...
+
 });
