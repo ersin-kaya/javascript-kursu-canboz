e8314deb609d36c7db65f6f0c69b7180b818ef32
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-29 00:03:38 +0300

    Promise - başlangıç

---
+++ Bolum-11/7-Promise/script.js
@@ -0,0 +1,26 @@
+const getTodos = (resource, callback) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        } else if (request.readyState === 4) {
+            callback('Başarılı cevap alamadık...', undefined);
+        }
+    });
+
+    request.open('GET', resource);
+    request.send();
+};
+
+getTodos('example/can.json', (err, data) => {
+
+    console.log(data);
+    getTodos('example/cem.json', (err, data) => {
+        console.log(data);
+    });
+
+    getTodos('example/tolga.json', (err, data) => {
+        console.log(data);
+    });
+});
