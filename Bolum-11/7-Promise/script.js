b653fc803e4bef15d21c58fdf424e4c38d1f85d4
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-29 00:24:08 +0300

    Promise - tek bir json dosyasına erişmeyi başardık

--- Bolum-11/7-Promise/script.js
+++ Bolum-11/7-Promise/script.js
@@ -1,36 +1,38 @@
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback('Başarılı cevap alamadık...', undefined);
-        }
-    });
+const getTodos = (resource) => { //artık geriye callback döndürmeyecek
 
-    request.open('GET', resource);
-    request.send();
-};
-
-// getTodos('example/can.json', (err, data) => {
-
-//     console.log(data);
-//     getTodos('example/cem.json', (err, data) => {
-//         console.log(data);
-//     });
-
-//     getTodos('example/tolga.json', (err, data) => {
-//         console.log(data);
-//     });
-// });
-
-const getIt = () => {
+    //şimdi örneğimizi promise kullanarak gerçekleştirelim
     return new Promise((resolve, reject) => {
-        resolve("Başarılı Data");
-        reject("Başarısız İstek");
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+                // callback(undefined, data);
+            } else if (request.readyState === 4) {
+                reject("Başarılı cevap alamadık...");
+                // callback('Başarılı cevap alamadık...', undefined);
+            }
+        });
+
+        request.open('GET', resource);
+        request.send();
     });
+
+
 };
+
+getTodos('example/can.json').then(data => {
+    console.log(data);
+}).catch((err) => {
+    console.log(err);
+});
+
+// const getIt = () => {
+//     return new Promise((resolve, reject) => {
+//         resolve("Başarılı Data");
+//         reject("Başarısız İstek");
+//     });
+// };
 
 // getIt().then(
 //     data => {
@@ -39,9 +41,9 @@ const getIt = () => {
 //         console.log("Başarısız", err);
 //     });
 
-getIt().then(   //hata için catch'i kullanıyoruz
-    data => {
-        console.log("Başarılı", data);
-    }).catch(err => {
-        console.log("Başarısız", err);
-    });
+// getIt().then(   //hata için catch'i kullanıyoruz
+//     data => {
+//         console.log("Başarılı", data);
+//     }).catch(err => {
+//         console.log("Başarısız", err);
+//     });
