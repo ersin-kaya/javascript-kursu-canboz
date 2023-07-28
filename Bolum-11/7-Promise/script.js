37f3b25f896bcf87a77b0372b38fc68f1ca78853
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-29 00:14:25 +0300

    Promise - temel kullanım(gerçekçi değil)

--- Bolum-11/7-Promise/script.js
+++ Bolum-11/7-Promise/script.js
@@ -13,14 +13,28 @@ const getTodos = (resource, callback) => {
     request.send();
 };
 
-getTodos('example/can.json', (err, data) => {
+// getTodos('example/can.json', (err, data) => {
 
-    console.log(data);
-    getTodos('example/cem.json', (err, data) => {
-        console.log(data);
-    });
+//     console.log(data);
+//     getTodos('example/cem.json', (err, data) => {
+//         console.log(data);
+//     });
 
-    getTodos('example/tolga.json', (err, data) => {
-        console.log(data);
+//     getTodos('example/tolga.json', (err, data) => {
+//         console.log(data);
+//     });
+// });
+
+const getIt = () => {
+    return new Promise((resolve, reject) => {
+        // resolve("Başarılı Data");    //bu açıkken çıktı: başarılı başarılı data //bunu kapatınca çıktı: başarısız başarısız istek // he bu böyle mi kullanılıyor hayır, mdn'de ki örnek kullanıma bakalım
+        reject("Başarısız İstek");
     });
-});
+};
+
+getIt().then(
+    data => {
+        console.log("Başarılı", data);
+    }, err => {
+        console.log("Başarısız", err);
+    });
