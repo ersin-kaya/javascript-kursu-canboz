dfb340ec83a5a24f9be7cdde11adc571413441d9
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-29 01:58:58 +0300

    Async Await - status kodu kontrolü ve bitiş

--- Bolum-11/10-AsyncAwait/script.js
+++ Bolum-11/10-AsyncAwait/script.js
@@ -8,7 +8,10 @@ const getTodos = async () => {
 
     //4. async'i kullandığımız zaman, herhangi bir backend olsun bir api olsun işte normal lokal bir dosyanıza istek attığınız zaman yani fecth etme (çekme) işlemi yaptığınız zaman Await'i kullanmanız lazım..
 
-    let response = await fetch('example/can.json') //5. await bizim bildiğimiz promise chain işini yapıyor, yani bu fetch işlemi olana kadar (can.json çekilene kadar), (burada tanımladığımız)response değişkenine atamasını yapmıyor. Ne zamanki fetch ile işi bitti (oradan bir promise döndü) örneğin 0.05 saniyede mi döndü, 0.05 saniye bekliyor ne zamanki promise döndü, tak promise'i bu response'a atıyoruz. 
+    let response = await fetch('example/can.json') //5. await bizim bildiğimiz promise chain işini yapıyor, yani bu fetch işlemi olana kadar (can.json çekilene kadar), (burada tanımladığımız)response değişkenine atamasını yapmıyor. Ne zamanki fetch ile işi bitti (oradan bir promise döndü) örneğin 0.05 saniyede mi döndü, 0.05 saniye bekliyor ne zamanki promise döndü, tak promise'i bu response'a atıyoruz.
+    if (response.status !== 200) {
+        throw new Error("Doğru endpoint'e istek atmadınız");
+    }
     const data = await response.json(); //6. burada da başında await kullanmamız lazım, neden? çünkü bu da bir promise döndürüyor, ve bunu(response) çekerkende fonksiyonumuzun başında async kullandığımız için burada da await'i kullanmamız lazım
     // console.log(data);
     //7. peki biz bunu çağırdıktan sonra çağırdığımız yerde kullanmak istersek nasıl yapacağız? bunu bir kere return etmemiz lazım, buradaki neyi promise'i neden? async bir promise döndürüyor!!!
