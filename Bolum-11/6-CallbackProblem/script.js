affa98b44ce14d80f8fb17942e4efd1f9ff9542c
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-28 23:44:26 +0300

    Callback Problem - diğer json dosyalarına da erişildi fakat yöntem hoş değil

--- Bolum-11/6-CallbackProblem/script.js
+++ Bolum-11/6-CallbackProblem/script.js
@@ -16,5 +16,12 @@ const getTodos = (resource, callback) => {
 getTodos('example/can.json', (err, data) => { //2. o halde buradaki callbackfunction yapısıda değişmiş olacak. İlk parametre bizim dosyayı açacağımız yer olacak.
 
     console.log(data); //3.artık bir error'a gerek yok diyor...
+    //5. veriler geldi fakat bu yöntem kullanım olarak hoş değil, ilkinin bitmesini bekleyecek çünkü artık data buraya(hemen üstteki console.log(data); satırı) gelecek, neden bekleyecek? datanın ulaşması için readyState===4'e girmesi ve status 200 cevabını almış olması lazım, yani ilk istek bitecek sonra alttaki getTodos() çalışacak, ikinci istekte bitecek daha sonra hemen altındaki çalışacak, işte Promise kullanımıyla biz bu problemi aşabiliyoruz...
+    getTodos('example/cem.json', (err, data) => { //4. diğer json dosyalarına da ulaşmak istiyoruz o yüzden şu an bunu yapalım diyoruz
+        console.log(data);
+    });
 
+    getTodos('example/tolga.json', (err, data) => {
+        console.log(data);
+    });
 });
