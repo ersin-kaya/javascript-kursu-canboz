c9a2ce2389d29268a7c43d5749ddf8742163152d
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-29 01:12:53 +0300

    Fetch - bir api'a istek atma

--- Bolum-11/9-Fetch/script.js
+++ Bolum-11/9-Fetch/script.js
@@ -1,4 +1,5 @@
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
+//bir api'a istek attığımızda böyle
+fetch('https://jsonplaceholder.typicode.com/todos/1') //1. fetch geriye promise dönüyor
+    .then(response => response.json()) //2. response bizim resolve'umuz yani geriye doğru data döndüğünde dönen şey
     .then(json => console.log(json))
+
