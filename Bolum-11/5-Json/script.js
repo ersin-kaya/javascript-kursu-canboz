2dc40adc1e6a9f6b5972ba438bee1aee4f3bf352
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-28 23:19:11 +0300

    Json - başlangıç

---
+++ Bolum-11/5-Json/script.js
@@ -0,0 +1,21 @@
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText); //ilk parametre error, burada hata olmadığı için undefined
+        } else if (request.readyState === 4) {
+            callback('Başarılı cevap alamadık...', undefined); //burada da data undefined
+        }
+    });
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    request.send();
+};
+
+getTodos((err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
