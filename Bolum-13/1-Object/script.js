06ed29aeb33650818582b3f921e4ec095ad9718a
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-30 23:38:26 +0300

    Object - kötü kullanım(class kullanmadan object oluşturma)

---
+++ Bolum-13/1-Object/script.js
@@ -0,0 +1,28 @@
+const userOne = {
+    username: 'Can',
+    email: 'can@hotmail.com',
+    login() {
+        console.log('Can Boz giriş yaptı');
+    },
+    logout() {
+        console.log('Can Boz çıkış yaptı');
+    },
+};
+
+console.log(userOne.email, userOne.username);
+userOne.login();
+
+const userTwo = {
+    username: 'Cem',
+    email: 'cem@hotmail.com',
+    login() {
+        console.log('Cem giriş yaptı');
+    },
+    logout() {
+        console.log('Cem çıkış yaptı');
+    },
+};
+
+
+console.log(userTwo.email, userTwo.username);
+userTwo.login();
